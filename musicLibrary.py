816632405995df7668c0bd2bae76fe8611315fdf
Raghavthakur00 <118159919+Raghavthakur00@users.noreply.github.com>
2025-06-16 20:44:33 +0530

    Create musicLibrary.py

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "hanuman chalisa": "https://www.youtube.com/watch?v=AETFvQonfV8",
+    "bhul": "https://www.youtube.com/watch?v=Q5qiXjOj_Uc&list=WL&index=23",
+    "baby": "https://www.youtube.com/watch?v=kffacxfA7G4",
+    "shape of you": "https://www.youtube.com/watch?v=JGwWNGJdvx8"
+}
